6ef6037a91daef706a74a729238cc416cf6e3c67
Jake Oney <jakeoney09@gmail.com>
2015-04-30 18:36:54 -0500

    Works. Was calculating p_w_given_l incorrectly.

--- Smart5/src/NaiveBayesClassifierImpl.java
+++ Smart5/src/NaiveBayesClassifierImpl.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 /**
  * Your implementation of a naive bayes classifier. Please implement all four methods.
@@ -15,10 +16,8 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	private Map<Label, Map<String, Integer> > wordTypeCount;
 	private Map<Label, Integer> labelTokenCount;
 	private int numInstance;
-	//private int numTotalToken;
 	private int vocabularySize;
 	
-	/*Currently not doing anything with labelTokenCount*/
 	@Override
 	public void train(Instance[] trainingData, int v) {
 		//Implement
@@ -27,7 +26,8 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 		this.labelCount = new HashMap<Label, Integer>();
 		this.wordTypeCount = new HashMap<Label, Map<String,Integer>>();
 		this.labelTokenCount = new HashMap<Label, Integer>();
-		int tmp = 0;
+		this.labelTokenCount.put(Label.HAM, 0);
+		this.labelTokenCount.put(Label.SPAM, 0);
 		
 		for(Instance inst : trainingData){
 			this.numInstance++;
@@ -39,7 +39,7 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 			
 			//update the wordTypeCount
 			for(String word : inst.words){
-				tmp++;
+				this.labelTokenCount.put(inst.label, this.labelTokenCount.get(inst.label)+1);
 				if(wordTypeCount.containsKey(inst.label)){
 					Map<String, Integer> innerMap = wordTypeCount.get(inst.label);
 					if(innerMap.containsKey(word)){
@@ -59,16 +59,6 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 				}
 			}
 		}
-		/*System.out.println(labelCount.get(Label.HAM));
-		System.out.println(labelCount.get(Label.SPAM));
-		System.out.println(wordTypeCount.get(Label.HAM).entrySet().size() + " ham words");
-		System.out.println(wordTypeCount.get(Label.SPAM).entrySet().size() + " spam words");
-		System.out.println(wordTypeCount.get(Label.HAM).entrySet().size() + wordTypeCount.get(Label.SPAM).entrySet().size() + " total unique words");
-		System.out.println("Vocab size is " + (this.vocabularySize - (wordTypeCount.get(Label.HAM).entrySet().size() + wordTypeCount.get(Label.SPAM).entrySet().size()))+" greater that num words above.\n" + this.vocabularySize + " vocab size");
-		
-		System.out.println(tmp + " total words");
-		System.exit(0);
-		*/
 	}
 
 	/**
@@ -84,14 +74,22 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	 * Returns the smoothed conditional probability of the word given the label,
 	 * i.e. P(word|SPAM) or P(word|HAM)
 	 */
+	//CHANGED HOW I WAS DOING THIS PREVIOUSLY. SHOULD BE WORKING NOW. PREVIOUSLY WASN'T USING LABELTOKENCOUNT
 	@Override
 	public double p_w_given_l(String word, Label label) {
 		// Implement
-		//word not in dictionary
-		if(wordTypeCount.get(word) == null){
-			
+		int labelWordCount = 0;
+		
+		//count total occurrences
+		for(Entry<Label, Map<String, Integer>> lab : wordTypeCount.entrySet()){
+			if(lab.getValue().containsKey(word)){
+				if(lab.getKey().equals(label)){
+					labelWordCount += lab.getValue().get(word);
+				}
+			}
 		}
-		return 0.0;
+		//return (double) ((labelWordCount + delta) / (double)((delta*vocabularySize) + totalCount));
+		return (double) ((labelWordCount + delta) / (double)((delta*vocabularySize) + this.labelTokenCount.get(label)));
 	}
 	
 	/**
@@ -100,10 +98,28 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	@Override
 	public ClassifyResult classify(String[] words) {
 		// Implement
-		//for(String word : words){
-			
-		//}
-		return null;
+		ClassifyResult result = new ClassifyResult();
+		Label currLabel = Label.HAM;
+		double prob = 0.0;
+		for(String word : words){
+			prob += Math.log(p_w_given_l(word, currLabel));
+		}
+		prob += Math.log(this.p_l(currLabel));
+		result.log_prob_ham = prob;
+		
+		prob = 0.0;
+		currLabel = Label.SPAM;
+		for(String word : words){
+			prob += Math.log(p_w_given_l(word, currLabel));
+		}
+		prob += Math.log(this.p_l(currLabel));
+		result.log_prob_spam = prob;
+		
+		if(result.log_prob_ham > result.log_prob_spam)
+			result.label = Label.HAM;
+		else
+			result.label = Label.SPAM;
+		return result;
 	}
 	
 	/**
@@ -113,6 +129,26 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	public ConfusionMatrix calculateConfusionMatrix(Instance[] testData)
 	{
 		// Implement
-		return null;
+		ConfusionMatrix mat = new ConfusionMatrix(0,0,0,0);
+		for(Instance inst : testData){
+			ClassifyResult r = this.classify(inst.words);
+			if(inst.label.equals(Label.HAM)){
+				if(r.label.equals(Label.HAM)){
+					mat.trueNegatives += 1;
+				}
+				else{
+					mat.falsePositives += 1;
+				}
+			}
+			else{
+				if(r.label.equals(Label.SPAM)){
+					mat.truePositives += 1;
+				}
+				else{
+					mat.falseNegatives += 1;
+				}
+			}
+		}
+		return mat;
 	}
 }
